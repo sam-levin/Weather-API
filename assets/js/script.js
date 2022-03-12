6e2dfb8ecfbb653701d04438ae3d754cbe74c721
sam-levin <sam.ky.levin@gmail.com>
2022-03-12 10:42:38 -1000

    initial styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-var 
+// when the button is clicked
+// api is called
+// new weather thing is created
+var weatherBlockContainer = $("#weatherBlockContainer")
+var inputCity = "Penisville"
+var createWeatherReport = function(inputCity) {
+    var weatherBlock = $("<div>").addClass("weather-block")
+    .text(inputCity);
+    weatherBlockContainer.append(weatherBlock)
+}
+
+createWeatherReport(inputCity);
